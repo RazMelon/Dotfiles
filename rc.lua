dc6f21b754c1a11e90a91c8ef2b33685cc55288e
razmelon <gryffin1352@gmail.com>
2024-06-06 17:12:14 -0400

    add smaller useless gap in rc.lua for laptop, given small screen real estate.

--- rc.lua
+++ rc.lua
@@ -596,7 +596,7 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- }}}
 
 --custom configs
-beautiful.useless_gap=16
+beautiful.useless_gap=8
 client.connect_signal("request::manage", function(c)
     c.shape = function(cr, w, h)
         gears.shape.rounded_rect(cr, w, h, 12)
