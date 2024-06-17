6bfb3280240bbc2782459e1327ac4b5c3bbff1ed
razmelon <gryffin1352@gmail.com>
2024-06-17 16:16:33 -0400

    add changes to rc.lua to allow for floating windows with a layout change

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -135,6 +135,11 @@ _G.packer_plugins = {
     path = "/home/razmelon/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
+  ["nvim-gdb"] = {
+    loaded = true,
+    path = "/home/razmelon/.local/share/nvim/site/pack/packer/start/nvim-gdb",
+    url = "https://github.com/sakhnik/nvim-gdb"
+  },
   ["nvim-lspconfig"] = {
     loaded = true,
     path = "/home/razmelon/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
